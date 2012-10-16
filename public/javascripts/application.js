58a67f62a56142fb5774fffd7c59bb50201d5004
Emmanuel Medina <minardimedia@gmail.com>
2012-10-16 16:40:24 -0500

    Adding Raw Code View

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
 
+  $('.toggle-view').click(function() {
+    if ( $('.typography').is(':visible') ) {
+      $('.typography').hide();
+      $('.code-block').show();
+      $(this).text('View example')
+
+    } else {
+      $('.typography').show();
+      $('.code-block').hide();
+      $(this).text('View raw code')
+
+    }
+  })
 });
